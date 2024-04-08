8ce1974b0f3ba6b80bd4846777a74116d3d8f79b
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-04-08 14:43:12 -0300

    numeros armstrong

--- src/implementacao/logica/numeros-armstrong.ts
+++ src/implementacao/logica/numeros-armstrong.ts
@@ -1,4 +1,21 @@
-function numerosArmstrong(numero: number) {
-    return null;
-}
+function numerosArmstrong (numero: number): string {
+    let nstring = numero.toString(); 
+    let operacao = 0;
+    let soma = 0;
+  
+  
+    for(let i = 0; i < nstring.length; i++){
+     let digito: number = parseInt(nstring.charAt(i)); 
+     operacao = digito ** nstring.length;
+     soma += operacao;
+      
+    }
+     if (soma === numero) {
+       return "Este eh um numero de Armstrong!"
+     }
+     else {
+       return "Este nao eh um numero de Armstrong!"
+  }
+  } 
+
 module.exports = numerosArmstrong;
