2aac79c5d211da4a21031d99d2f0cb3671270f37
Cecília Barbosa <105225682+barbo347@users.noreply.github.com>
2024-04-08 11:01:42 -0300

    Update fizz-buzz

--- src/implementacao/logica/fizz-buzz.ts
+++ src/implementacao/logica/fizz-buzz.ts
@@ -1,4 +1,15 @@
-function fizzBuzz(numero: number) {
-    return null;
-}
-module.exports = fizzBuzz;
+function fizzbuzz(numero: number): string {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+      return "FizzBuzz";
+    }
+    else if (numero % 3 === 0) {
+      return "Fizz"
+    }
+    else if (numero % 5 === 0) {
+      return "Buzz"
+    }
+    else {
+      return numero.toString();
+    }
+  }
+module.exports = fizzbuzz;
